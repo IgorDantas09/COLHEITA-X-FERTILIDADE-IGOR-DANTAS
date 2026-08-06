30e621943fc2f893ff48dd1f2deff2422aa92990
IgorDantas09 <igor.dantas1015@gmail.com>
2026-08-06 10:05:22 -0400

    Delete libs/agrogeo/agrogeo directory

--- libs/agrogeo/agrogeo/__init__.py
+++
@@ -1,6 +0,0 @@
-from .pipeline import ProcessingConfig, process_yield_map
-from .report import create_yield_pdf
-from .fertility import FertilityConfig, inspect_excel, process_fertility
-from .fertility_report import create_fertility_pdf
-
-__all__ = ["ProcessingConfig","process_yield_map","create_yield_pdf","FertilityConfig","inspect_excel","process_fertility","create_fertility_pdf"]
